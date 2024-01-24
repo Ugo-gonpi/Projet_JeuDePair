bf54a7aeb64625b704ded6734d5452948cc5264a
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-01-24 16:42:12 +0100

    corrected some details

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+let card1 = document.querySelectorAll(".flip-card-inner1")
+let card2 = document.querySelectorAll(".flip-card-inner2")
+let card3 = document.querySelectorAll(".flip-card-inner3")
+let card4 = document.querySelectorAll(".flip-card-inner4")
+
+for (let index = 0; index < card1.length; index++) {
+    card1[index].addEventListener('click', () => {
+        card1[index].classList.add("retourner")
+    })
+}
+
+for (let index = 0; index < card3.length; index++) {
+    card2[index].addEventListener('click', () => {
+        card2[index].classList.add("retourner")
+    })
+}
+
+for (let index = 0; index < card3.length; index++) {
+    card3[index].addEventListener('click', () => {
+        card3[index].classList.add("retourner")
+    }) 
+}
+
+for (let index = 0; index < card4.length; index++) {
+    card4[index].addEventListener('click', () => {
+        card4[index].classList.add("retourner")
+    }) 
+}
