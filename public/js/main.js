ea974d261558cd99e029de5db09671ff0b5d7756
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-01-25 08:48:43 +0100

    did what i could

--- public/js/main.js
+++ public/js/main.js
@@ -5,24 +5,24 @@ let card4 = document.querySelectorAll(".flip-card-inner4")
 
 for (let index = 0; index < card1.length; index++) {
     card1[index].addEventListener('click', () => {
-        card1[index].classList.add("retourner")
+        card1[index].classList.toggle("retourner")
     })
 }
 
 for (let index = 0; index < card3.length; index++) {
     card2[index].addEventListener('click', () => {
-        card2[index].classList.add("retourner")
+        card2[index].classList.toggle("retourner")
     })
 }
 
 for (let index = 0; index < card3.length; index++) {
     card3[index].addEventListener('click', () => {
-        card3[index].classList.add("retourner")
+        card3[index].classList.toggle("retourner")
     }) 
 }
 
 for (let index = 0; index < card4.length; index++) {
     card4[index].addEventListener('click', () => {
-        card4[index].classList.add("retourner")
+        card4[index].classList.toggle("retourner")
     }) 
 }
